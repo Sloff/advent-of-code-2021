8ef402bf2f0a41cae39ea763041d1c9ffe4e9f74
Ettienne Pitts <ettpitts@pm.me>
2021-12-02 10:13:04 +0200

    feat: Day 2 solution

---
+++ day-2/src/main.rs
@@ -0,0 +1,159 @@
+use std::fs;
+
+type Instruction<'a> = (&'a str, i32);
+type Instructions<'a> = Vec<Instruction<'a>>;
+
+fn main() {
+    let input = fs::read_to_string("day-2/input.txt").unwrap();
+
+    let instructions: Instructions = process_input(&input[..]);
+
+    let mut position = Position::new();
+    position.process_instructions(&instructions);
+
+    let mut position_with_aim = Position::new_aim();
+    position_with_aim.process_instructions(&instructions);
+
+    println!("Final position: {}", position.get_position());
+    println!(
+        "Final position advanced: {}",
+        position_with_aim.get_position()
+    );
+}
+
+fn process_input(input: &str) -> Vec<(&str, i32)> {
+    input
+        .lines()
+        .map(|x| {
+            let mut input_iter = x.split_whitespace();
+            (
+                input_iter.next().unwrap(),
+                input_iter.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect()
+}
+
+#[derive(Copy, Clone)]
+enum PositionType {
+    Naive,
+    Aim,
+}
+
+struct Position {
+    horizontal: i32,
+    depth: i32,
+    aim: i32,
+    position_type: PositionType,
+}
+
+impl Position {
+    fn new() -> Position {
+        Position {
+            horizontal: 0,
+            depth: 0,
+            aim: 0,
+            position_type: PositionType::Naive,
+        }
+    }
+
+    fn new_aim() -> Position {
+        Position {
+            horizontal: 0,
+            depth: 0,
+            aim: 0,
+            position_type: PositionType::Aim,
+        }
+    }
+
+    fn process_position(&mut self, instruction: &Instruction) {
+        *self = match self.position_type {
+            PositionType::Naive => match instruction {
+                ("forward", x) => Position {
+                    horizontal: self.horizontal + x,
+                    ..*self
+                },
+                ("down", x) => Position {
+                    depth: self.depth + x,
+                    ..*self
+                },
+                ("up", x) => Position {
+                    depth: self.depth - x,
+                    ..*self
+                },
+                _ => unreachable!(),
+            },
+            PositionType::Aim => match instruction {
+                ("forward", x) => Position {
+                    horizontal: self.horizontal + x,
+                    depth: self.depth + (self.aim * x),
+                    ..*self
+                },
+                ("down", x) => Position {
+                    aim: self.aim + x,
+                    ..*self
+                },
+                ("up", x) => Position {
+                    aim: self.aim - x,
+                    ..*self
+                },
+                _ => unreachable!(),
+            },
+        };
+    }
+
+    fn process_instructions(&mut self, instructions: &Instructions) {
+        instructions
+            .into_iter()
+            .for_each(|instruction| self.process_position(instruction));
+    }
+
+    fn get_position(&self) -> i32 {
+        self.horizontal * self.depth
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn input() {
+        assert_eq!(
+            process_input("forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"),
+            vec![
+                ("forward", 5),
+                ("down", 5),
+                ("forward", 8),
+                ("up", 3),
+                ("down", 8),
+                ("forward", 2)
+            ]
+        );
+    }
+
+    #[test]
+    fn position() {
+        let mut pos = Position::new();
+        pos.process_position(&("forward", 5));
+        assert_eq!((pos.horizontal, pos.depth), (5, 0));
+    }
+
+    #[test]
+    fn result() {
+        let mut pos = Position::new();
+        pos.process_instructions(&process_input(
+            "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2",
+        ));
+        assert_eq!(pos.get_position(), 150);
+    }
+
+    #[test]
+    fn result2() {
+        let mut pos = Position::new_aim();
+        pos.process_instructions(&process_input(
+            "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2",
+        ));
+        assert_eq!(pos.get_position(), 900);
+    }
+}
